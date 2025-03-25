42992a6a2a9f2f9598bb753675c3c1473ece92c8
Felipe Alexandre <felipeadar108@gmail.com>
2025-03-25 15:30:41 -0300

    refactor: update Prettier configuration, remove pre-commit hook, and upgrade Vite version

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Tasks from './components/Tasks'
-import Header from './components/Header'
+import Header from "./components/Header"
 
 function App() {
   return (
     <div>
-    <Header />
-    <Tasks />
-  </div>
-  );
+      <Header />
+      <Tasks />
+    </div>
+  )
 }
 
-export default App;
+export default App
