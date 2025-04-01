4681bdfda38e3803c7fbcdb5a6810ef7e46fa2be
Felipe Alexandre <felipeadar108@gmail.com>
2025-04-01 14:32:30 -0300

    feat: add new SVG icons for task management and update icons index

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,10 @@
 import Button from "./Button"
-import TrashIcon from "../assets/icons/trash-icon.svg?react"
-import AddIcon from "../assets/icons/add-icon.svg?react"
+import TrashIcon from "../assets/icons/trash.svg?react"
+import AddIcon from "../assets/icons/add.svg?react"
+import SunIcon from "../assets/icons/sun.svg?react"
+import CloudIcon from "../assets/icons/cloud-sun.svg?react"
+import MoonIcon from "../assets/icons/moon.svg?react"
+import TASKS from "../data/tasks"
 
 const Tasks = () => {
   return (
@@ -22,6 +26,30 @@ const Tasks = () => {
             <AddIcon />
             Adicionar Tarefa
           </Button>
+        </div>
+      </div>
+
+      {/* LISTA DE TAREFAS */}
+      <div className="rounded-xl bg-white p-6">
+        <div className="space-y-3">
+          <div className="flex gap-2 border-b border-[#E5E5E5] pb-1 text-sm">
+            <SunIcon />
+            <p className="text-[#9A9C9F]">Manhã</p>
+          </div>
+        </div>
+
+        <div className="my-6 space-y-3">
+          <div className="flex gap-2 border-b border-[#E5E5E5] pb-1 text-sm">
+            <CloudIcon />
+            <p className="text-[#9A9C9F]">Tarde</p>
+          </div>
+        </div>
+
+        <div className="space-y-3">
+          <div className="flex gap-2 border-b border-[#E5E5E5] pb-1 text-sm">
+            <MoonIcon />
+            <p className="text-[#9A9C9F]">Noite</p>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/data/tasks.js
@@ -0,0 +1,25 @@
+const TASKS = [
+  {
+    id: 1,
+    title: "Tarefa 1",
+    description: "Descrição da tarefa 1",
+    time: "morning",
+    status: "done",
+  },
+  {
+    id: 2,
+    title: "Tarefa 2",
+    description: "Descrição da tarefa 2",
+    time: "afternoon",
+    status: "in_progress",
+  },
+  {
+    id: 3,
+    title: "Tarefa 3",
+    description: "Descrição da tarefa 3",
+    time: "moon",
+    status: "not_started",
+  },
+]
+
+export default TASKS
