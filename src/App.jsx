99d70dba3bbe44b19540ffb0130a6ff0b0e491e8
Felipe Alexandre <felipeadar108@gmail.com>
2025-05-15 17:06:52 -0300

    style: update layout spacing in App and Tasks components

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Sidebar from "./components/Sidebar"
 
 function App() {
   return (
-    <div className="flex gap-9">
+    <div className="flex">
       <Sidebar />
       <Tasks />
     </div>
